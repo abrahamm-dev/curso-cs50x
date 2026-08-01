74f569181ecae8053a97936513ab30bc73dfc275
abrahamm-dev <contactabrahamarguello@gmail.com>
2026-07-31 20:03:58 -0400

    Mi primer script de IA para el CS50x sin credenciales expuestas

--- chat.py
+++ chat.py
@@ -1,16 +1,17 @@
+import os
 from openai import OpenAI
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-response = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "In one sentence, what is CS50?",
+# response = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "In one sentence, what is CS50?",
 
-        }
-    ],
-    model="gpt-4o-mini"
-)
+#         }
+#     ],
+#     model="gpt-4o-mini"
+# )
 
-print(response.choices[0].message.content)
+# print(response.choices[0].message.content)
