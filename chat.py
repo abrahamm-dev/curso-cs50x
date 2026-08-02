526d204724647c9052a11b138366a9e737be93f0
abrahamm-dev <contactabrahamarguello@gmail.com>
2026-08-02 11:17:12 -0400

    Se solucionaron los errores previos, consegui censurar el API_KEY (evidentemente reemplace el original por uno nuevo y consegui un modelo de IA que funciona correctamente para el progreso del proyecto

--- chat.py
+++ chat.py
@@ -1,17 +1,20 @@
 import os
-from openai import OpenAI
+import openai
 
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+client = openai.OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.environ.get("GROQ_API_KEY")
+)
 
-# response = client.chat.completions.create(
-#     messages=[
-#         {
-#             "role": "user",
-#             "content": "In one sentence, what is CS50?",
+user_prompt = input("Prompt:")
+system_prompt = "Limit your answer to one sentence."
 
-#         }
-#     ],
-#     model="gpt-4o-mini"
-# )
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt}
+    ]
+)
 
-# print(response.choices[0].message.content)
+print(response.choices[0].message.content)
